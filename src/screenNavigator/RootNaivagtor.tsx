35c163b916f3d5ca8dc0c0bcfab786562d8bba7a
vaibhav164 <vai1641996@gmail.com>
2024-03-03 00:11:03 +0530

    Updated RootNavigator.tsx

--- src/screenNavigator/RootNaivagtor.tsx
+++ src/screenNavigator/RootNaivagtor.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen/HomeScreen';
-import LoginScreen from '../screens/loginScreen/LoginScreen';
 import LoginStackNavigator from './LoginStackNavigator';
 import MainStackNavigator from './MainStackNavigator';
 const RootNavigator = () => {
