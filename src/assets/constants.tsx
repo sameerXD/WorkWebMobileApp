ceaeab200edc4f8a0f2b75950f8a59c32ce84192
vaibhav164 <vai1641996@gmail.com>
2024-03-06 12:36:11 +0530

    Added constant for ScreenHeight and Width and For Service card

---
+++ src/assets/constants.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Dimensions } from "react-native";
+
+export const ScreenWidth = Dimensions.get('window').width;
+export const ScreenHeight = Dimensions.get('window').height;
---
+++ src/screens/HomeScreen/HomeScreenConstants.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import WorkSpace from '../../assets/icons/workspace.svg'
+import WebThreadLogo from '../../assets/icons/webthread.svg'
+import VendorManagement from '../../assets/icons/vendorManagement.svg'
+import HRMangement from '../../assets/icons/HRManangement.svg'
+import WareHouse from '../../assets/icons/wareHouse.svg'
+
+export const serviceList =[
+    {name:'Workspace',icon:<WorkSpace />, id:1},
+    {name:'Project Management',icon:<WebThreadLogo />, id:2},
+    {name:'Expense Management',icon:<WorkSpace />, id:3},
+    {name:'Visitor Management',icon:<WorkSpace />, id:4},
+    {name:'Web Thread',icon:<WebThreadLogo />, id:5},
+    {name:'Customer Management',icon:<WorkSpace />, id:6},
+    {name:'Vendor Management',icon:<VendorManagement />, id:7},
+    {name:'Warehouse Mangenment',icon:<WareHouse />, id:8},
+    {name:'HR Mangenment',icon:<HRMangement />, id:9}
+]
