571b183bad8dc957bd7cfe0e72519b0d885343ac
vaibhav164 <vai1641996@gmail.com>
2024-03-07 12:48:55 +0530

    Used Profilecard and checkin checkoiut card in WorkspaceScreen

--- src/screens/WorkSpaceScreen/WorkSpaceScreen.tsx
+++ src/screens/WorkSpaceScreen/WorkSpaceScreen.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../../Components/Header';
+import { ProfileCard } from '../../Components/ProfileCard';
+import { CheckinRegularizeCard } from '../../Molecules/Atoms/CheckinRegularizeCard';
 export const WorkspaceScreen =()=>{
     return(
         <View style={{flex:1}}>
             <Header />
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Text>{'Workspace Screen'}</Text>
+        <View style={{flex:1, backgroundColor:'#fff', padding:'3%'}}>
+            <ProfileCard />
+            <Text style={styles.cardHeading}>{'Check In'}</Text>
+            <CheckinRegularizeCard />
         </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    cardHeading:{
+        fontSize:18,
+        fontWeight:'400',
+        color:'#000'
+    }
+})
