34c41b1250917cb5374018ed2f24b4018f37b5a8
vaibhav164 <vai1641996@gmail.com>
2024-03-12 14:49:53 +0530

    added base Url as constant in be imported in utils.tsx

--- src/assets/utils.tsx
+++ src/assets/utils.tsx
@@ -1,3 +1,5 @@
+import Config from "react-native-config";
+export const Base_url = Config.BASE_URL;
 export function validateEmail(email:string) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
