9d6b0015631780cac5b70d1534e6ebc0ba3b839e
vaibhav164 <vai1641996@gmail.com>
2024-03-12 18:35:46 +0530

    Added Interface to Service card

--- src/Components/ServiceCard.tsx
+++ src/Components/ServiceCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Text, TouchableWithoutFeedback, View } from 'react-native'
 import { ScreenWidth } from '../assets/constants'
-
-export const ServiceCard =({name, icon, handleServicePressed})=>{
+interface serviceCardProps {
+    name:string;
+    icon:string;
+    handleServicePressed:()=>void;
+    key:string;
+}
+export const ServiceCard =({name, icon, handleServicePressed, key}:serviceCardProps)=>{
     return(
         <TouchableWithoutFeedback onPress={handleServicePressed}>
         <View style={{ borderRadius:10, margin:'1%', borderColor:'#D9D9D9',borderWidth:1, width:ScreenWidth*0.3, height:ScreenWidth*0.32,alignItems:'center', padding:'2%', backgroundColor:'#fff'}}>
