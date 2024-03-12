d33eb8f809383e45534f2f9736052c3f5f13f3fa
vaibhav164 <vai1641996@gmail.com>
2024-03-12 18:29:43 +0530

    Added logic to logout from front end not from api

--- src/screens/LogOutScreen/LogOutScreen.tsx
+++ src/screens/LogOutScreen/LogOutScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import Button from '../../Components/Button'
 import { useDispatch, useSelector } from 'react-redux'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const LogOutScreen = ()=>{
     const dispatch = useDispatch();
     const loggedIn = useSelector(state=>state.user.isLoggedIn);
     const handleLogoutUser =()=>{
+        try {
+            AsyncStorage.setItem(
+               'token',
+               '',
+               );
+           } catch(error){
+               Alert.alert("Errors",error);
+           }
         dispatch({type:"USER_LOGGEDIN", payload:{ isLoggedIn: false}})
     }
     return(
