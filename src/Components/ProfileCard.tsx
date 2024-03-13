eef0e46273b83013a78b1f113b7ee5edef67b02d
vaibhav164 <vai1641996@gmail.com>
2024-03-13 17:09:46 +0530

    Formated Profile Card and Added Place holder image for Profile Image

--- src/Components/ProfileCard.tsx
+++ src/Components/ProfileCard.tsx
@@ -1,47 +1,55 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { ScreenHeight, ScreenWidth } from '../assets/constants'
+import Home from 'react-native-vector-icons/FontAwesome';
 import { useSelector } from 'react-redux';
-export const ProfileCard =()=>{
+export const ProfileCard = () => {
     const userData = useSelector(state => state.user.userData);
     return(
         <View style={styles.profileCard}>
-          <View style={styles.imageBox}>
-            <Image  source={{uri: userData?.trackerId?.personalDetails?.I?.profilePicture}} style={styles.image}/>
-          </View>  
-          <View style={styles.textBox}>
-            <Text style={styles.nameText}>{userData?.trackerId?.personalDetails?.I?.name}</Text>
-            <Text style={styles.userInfoText}>{userData?.designation}</Text>
-          </View>
+            <View style={styles.imageBox}>
+                {userData?.trackerId?.personalDetails?.I?.profilePicture ?
+                    <Image
+                        source={{ uri: userData?.trackerId?.personalDetails?.I?.profilePicture }}
+                        style={styles.image}
+                    />
+                    :
+                    <Home name='user-o' size={40} style={{ alignSelf: 'center' }} />
+                }
+            </View>
+            <View style={styles.textBox}>
+                <Text style={styles.nameText}>{userData?.trackerId?.personalDetails?.I?.name}</Text>
+                <Text style={styles.userInfoText}>{userData?.designation}</Text>
+            </View>
         </View>
     )
 }
 const styles = StyleSheet.create({
-profileCard:{
-    flexDirection:'row',
-    height:ScreenHeight*0.1
+    profileCard: {
+        flexDirection: 'row',
+        height: ScreenHeight * 0.1
     },
-imageBox:{
-    width:ScreenWidth*0.2, 
-    justifyContent:'center', 
-    alignItems:'center'
-},
-image:{
-    height:'70%', width:'70%', 
-    borderRadius:50
-},
-textBox:{
-    width:ScreenWidth*0.74, 
-    justifyContent:'center'
-},
-nameText:{
-    fontWeight:'400', 
-    fontSize:20, 
-    color:'#000'
-},
-userInfoText:{
-    marginTop:'2%', 
-    fontWeight:'400', 
-    color:'#000'
-}
+    imageBox: {
+        width: ScreenWidth * 0.2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        height: '70%', width: '70%',
+        borderRadius: 50
+    },
+    textBox: {
+        width: ScreenWidth * 0.74,
+        justifyContent: 'center'
+    },
+    nameText: {
+        fontWeight: '400',
+        fontSize: 20,
+        color: '#000'
+    },
+    userInfoText: {
+        marginTop: '2%',
+        fontWeight: '400',
+        color: '#000'
+    }
 })
