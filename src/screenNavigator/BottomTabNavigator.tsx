b57f274ffc953c600ea90dab5ff813d0ed72630a
vaibhav164 <vai1641996@gmail.com>
2024-03-13 17:10:08 +0530

    Added Leave Screen to Bottom Tab Navigator

--- src/screenNavigator/BottomTabNavigator.tsx
+++ src/screenNavigator/BottomTabNavigator.tsx
@@ -7,6 +7,7 @@ import Icon from 'react-native-vector-icons/Fontisto';
 import OfficeIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import DocumetIcon from 'react-native-vector-icons/AntDesign';
 import { LogOutScreen } from '../screens/LogOutScreen/LogOutScreen';
+import { LeaveScreen } from '../screens/HomeScreen/leaveScreen';
 const Tab = createBottomTabNavigator();
 
 export const BottomTabs=()=> {
@@ -19,7 +20,7 @@ export const BottomTabs=()=> {
       <Tab.Screen options={{tabBarIcon:()=><Home size={30} color={'#B4B4B8'} name='home'/>,  tabBarStyle: {paddingBottom:'2.5%',height:'7.5%'}}} name="Home" component={WorkspaceScreen} />
       <Tab.Screen options={{tabBarIcon:()=><OfficeIcon size={30} color={'#B4B4B8'} name='office-building-marker'/>}} name="Attendance" component={HomeScreen} />
       <Tab.Screen options={{tabBarIcon:()=><DocumetIcon size={30} color={'#B4B4B8'} name='copy1'/>}} name="Documents" component={HomeScreen} />
-      <Tab.Screen options={{tabBarIcon:()=><Icon size={30} color={'#B4B4B8'} name='holiday-village'/>}} name="Leaves" component={HomeScreen} />
+      <Tab.Screen options={{tabBarIcon:()=><Icon size={30} color={'#B4B4B8'} name='holiday-village'/>}} name="Leaves" component={LeaveScreen} />
       <Tab.Screen options={{tabBarIcon:()=><Home size={30} color={'#B4B4B8'} name='user'/>}} name="Profile" component={LogOutScreen} />
     </Tab.Navigator>
   );
