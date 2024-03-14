cf0280d471f7a749c70caf477d02d992e93d96de
vaibhav164 <vai1641996@gmail.com>
2024-03-14 15:58:53 +0530

    Added Loading feature in Button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { ActivityIndicator, Text, TouchableWithoutFeedback, View } from 'react-native';
 interface ButtonProps {
     title: string;
-    handleSubmit: ()=>void;
+    handleSubmit: () => void;
     color?: string;
-    size: 'l'|'m'|'sm';
+    size: 'l' | 'm' | 'sm';
     ghost?: boolean;
+    isLoading?: boolean;
 }
-function Button(props:ButtonProps) {
-    const { title, handleSubmit, color, size, ghost } = props;
+function Button(props: ButtonProps) {
+    const { title, handleSubmit, color, size, ghost, isLoading } = props;
     return (
-        <TouchableWithoutFeedback onPress={handleSubmit}>
-            <View style={{ width: size == 'l'?'95%':size == 'm' ? '100%' : '100%', backgroundColor: ghost ? '#fff' : color ? color :'#673AB7', borderRadius: size == 'l' ? 30 : 10, justifyContent: 'center', alignItems: 'center', borderColor: ghost ? '#673AB7':'#fff', borderWidth: 1 , paddingVertical:size == 'l' ? '3%':'6%'}}>
-                <Text style={{ color: ghost ? '#673AB7':'#fff', fontSize: 20 }}>{title}</Text>
+        <TouchableWithoutFeedback disabled={isLoading} onPress={handleSubmit}>
+            <View style={{ width: size == 'l' ? '95%' : size == 'm' ? '100%' : '100%', backgroundColor: ghost ? '#fff' : color ? color : '#673AB7', borderRadius: size == 'l' ? 30 : 10, justifyContent: 'center', alignItems: 'center', borderColor: ghost ? '#673AB7' : '#fff', borderWidth: 1, paddingVertical: size == 'l' ? '3%' : '6%' }}>
+                {
+                    isLoading ?
+                        <ActivityIndicator
+                            animating={true}
+                            size='large'
+                            color='#fff'
+                        /> :
+                        <Text style={{ color: ghost ? '#673AB7' : '#fff', fontSize: 20 }}>{title}</Text>
+                }
             </View>
         </TouchableWithoutFeedback>
     );
