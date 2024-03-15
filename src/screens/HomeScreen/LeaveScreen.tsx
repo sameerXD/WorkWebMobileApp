754aba2b9e43258947bbf32f42895516f90a431f
vaibhav164 <vai1641996@gmail.com>
2024-03-15 15:11:47 +0530

    Updated UI screen for Leavescreen

--- src/screens/HomeScreen/LeaveScreen.tsx
+++ src/screens/HomeScreen/LeaveScreen.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { LogBox, StyleSheet, Text, View } from 'react-native'
 import { Header } from '../../Components/Header'
+import { LeaveCard } from '../../Components/LeaveCard'
+import { useSelector } from 'react-redux'
+import Button from '../../Components/Button'
 
 
 export const LeaveScreen =()=>{
+    const userData = useSelector(state => state.user.userData);
+    const leaveList = Object.entries(userData?.leave).map(([k,v]) => {return [k, v]});
+    
     return(
         <View style={styles.container}>
             <Header />
             <View style={styles.leaveContainer}>
                 <Text style={styles.title}>{'Leave Balance'}</Text>
+                <View style={{flexDirection:'row', height:'50%', justifyContent:'space-between'}}>
+                <LeaveCard leaveName={'Total'} leaveBalance={leaveList[0][1] + Math.ceil(Number(leaveList[1][1]))}/>
+                {leaveList.map(item=>{
+                    if(item[0] != 'casualLeave'){
+                    return <LeaveCard leaveName={item[0]} leaveBalance={item[1]}/>
+                    }})}
+
+                </View>
+                    <Button title={'Apply Leave'} handleSubmit={()=>{}} size='sm' />
             </View>
         </View>
     )
@@ -19,8 +34,10 @@ const styles = StyleSheet.create({
         backgroundColor:'#fff'
     },
     leaveContainer:{
-        flex:1,
-        padding:'2%'
+        // flex:1,
+        padding:'2%',
+        justifyContent:'space-between',
+        height:'40%'
     },
     title:{
         color:'#000',
