106f5f8b0dc0a3e836d564aa4f6cb76bb21aca47
vaibhav164 <vai1641996@gmail.com>
2024-03-20 11:06:45 +0530

    Added Leave status option in Constant file

--- src/assets/constants.tsx
+++ src/assets/constants.tsx
@@ -3,3 +3,6 @@ import { Dimensions } from "react-native";
 
 export const ScreenWidth = Dimensions.get('window').width;
 export const ScreenHeight = Dimensions.get('window').height;
+
+
+export const leaveHistoyStatusOptions = ['Select','Pending', 'Approved', 'Declined']
