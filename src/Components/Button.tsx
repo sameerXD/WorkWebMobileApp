a677c91f863049a4f694943aba6fdb54a73f1991
vaibhav164 <vai1641996@gmail.com>
2024-03-21 16:47:42 +0530

    formated code for Button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,31 +1,53 @@
-import React from 'react'
-import { ActivityIndicator, Text, TouchableWithoutFeedback, View } from 'react-native';
-import { ScreenWidth } from '../assets/constants';
+import React from 'react';
+import {
+  ActivityIndicator,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import {ScreenWidth} from '../assets/constants';
 interface ButtonProps {
-    title: string;
-    handleSubmit: () => void;
-    color?: string;
-    size: 'l' | 'm' | 'sm';
-    ghost?: boolean;
-    isLoading?: boolean;
+  title: string;
+  handleSubmit: () => void;
+  color?: string;
+  size: 'l' | 'm' | 'sm';
+  ghost?: boolean;
+  isLoading?: boolean;
 }
 function Button(props: ButtonProps) {
-    const { title, handleSubmit, color, size, ghost, isLoading } = props;
-    return (
-        <TouchableWithoutFeedback disabled={isLoading} onPress={handleSubmit}>
-            <View style={{ width: size == 'l' ? ScreenWidth*0.95 : size == 'm' ? ScreenWidth*0.4 : ScreenWidth*0.3, backgroundColor: ghost ? '#fff' : color ? color : '#673AB7', borderRadius: 10, justifyContent: 'center', alignItems: 'center', borderColor: ghost ? '#673AB7' : '#fff', borderWidth: 1, paddingVertical: '3%'}}>
-                {
-                    isLoading ?
-                        <ActivityIndicator
-                            animating={true}
-                            size='large'
-                            color='#fff'
-                        /> :
-                        <Text style={{ color: ghost ? '#673AB7' : '#fff', fontSize: size == 'l' ? 20 : size == 'm' ? 16 : 15 }}>{title}</Text>
-                }
-            </View>
-        </TouchableWithoutFeedback>
-    );
+  const {title, handleSubmit, color, size, ghost, isLoading} = props;
+  return (
+    <TouchableWithoutFeedback disabled={isLoading} onPress={handleSubmit}>
+      <View
+        style={{
+          width:
+            size == 'l'
+              ? ScreenWidth * 0.91
+              : size == 'm'
+              ? ScreenWidth * 0.4
+              : ScreenWidth * 0.3,
+          backgroundColor: ghost ? '#fff' : color ? color : '#673AB7',
+          borderRadius: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: ghost ? '#673AB7' : '#fff',
+          borderWidth: 1,
+          paddingVertical: '3%',
+        }}>
+        {isLoading ? (
+          <ActivityIndicator animating={true} size="large" color="#fff" />
+        ) : (
+          <Text
+            style={{
+              color: ghost ? '#673AB7' : '#fff',
+              fontSize: size == 'l' ? 20 : size == 'm' ? 16 : 15,
+            }}>
+            {title}
+          </Text>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
 export default Button;
