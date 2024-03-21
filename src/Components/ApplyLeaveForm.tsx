85041e8b0631d3866494024603944328a06ca026
vaibhav164 <vai1641996@gmail.com>
2024-03-21 11:00:48 +0530

    Formated Code for ApplyLeaveForm

--- src/Components/ApplyLeaveForm.tsx
+++ src/Components/ApplyLeaveForm.tsx
@@ -1,88 +1,110 @@
-import React from 'react'
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import Button from './Button'
+import React from 'react';
+import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import Button from './Button';
 interface ApplyFormProps {
-    title: string;
-    handleSubmit:()=>void;
+  title: string;
+  handleSubmit: () => void;
 }
-export const ApplyLeaveForm =({title, handleSubmit}:ApplyFormProps)=>{
-    return (
-        <View style={styles.formContainer}>
-            <ScrollView style={{flex:0.9, width:'100%'}}>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.inputTitle}>{"Type of Leave"}</Text>
-            <TextInput 
-                placeholder='Leave Type'
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"From Date"}</Text>
-            <TextInput 
-                placeholder='DD/MM/YYYY'
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"To Date"}</Text>
-            <TextInput 
-                placeholder='DD/MM/YYYY'
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"Apply To"}</Text>
-            <TextInput 
-                placeholder=''
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"CC To"}</Text>
-            <TextInput 
-                placeholder=''
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"Alt No"}</Text>
-            <TextInput 
-                placeholder=''
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                style={styles.inputStyle}/>
-            <Text style={styles.inputTitle}>{"Reason"}</Text>
-            <TextInput 
-                placeholder=''
-                placeholderTextColor={'#000'} 
-                onChangeText={val=>{console.log(val)}}
-                multiline={true}
-                style={styles.inputStyle}/>
-            </ScrollView>
-            <Button handleSubmit={handleSubmit} title='Apply' size='m'/>
-        </View>
-    )
-}
+export const ApplyLeaveForm = ({title, handleSubmit}: ApplyFormProps) => {
+  return (
+    <View style={styles.formContainer}>
+      <ScrollView style={{flex: 0.9, width: '100%'}}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.inputTitle}>{'Type of Leave'}</Text>
+        <TextInput
+          placeholder="Leave Type"
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'From Date'}</Text>
+        <TextInput
+          placeholder="DD/MM/YYYY"
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'To Date'}</Text>
+        <TextInput
+          placeholder="DD/MM/YYYY"
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'Apply To'}</Text>
+        <TextInput
+          placeholder=""
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'CC To'}</Text>
+        <TextInput
+          placeholder=""
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'Alt No'}</Text>
+        <TextInput
+          placeholder=""
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          style={styles.inputStyle}
+        />
+        <Text style={styles.inputTitle}>{'Reason'}</Text>
+        <TextInput
+          placeholder=""
+          placeholderTextColor={'#000'}
+          onChangeText={val => {
+            console.log(val);
+          }}
+          multiline={true}
+          style={styles.inputStyle}
+        />
+      </ScrollView>
+      <Button handleSubmit={handleSubmit} title="Apply" size="m" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    formContainer:{
-        height:'100%',
-        paddingVertical:'8%',
-        alignItems:'center',
-        backgroundColor:'#fff',
-        paddingHorizontal:'5%'
-    },
-    title:{
-        fontSize:20,
-        color:'#000',
-        fontWeight:'600'
-    },
-    inputStyle:{
-        borderWidth:1,
-        width:'100%',
-        borderRadius:10,
-        borderColor:'#673AB7',
-        marginVertical:'2%',
-        paddingHorizontal:'3%'
-    },
-    inputTitle:{
-        color:'#000',
-        alignSelf:'flex-start',
-        fontWeight:'600'
-    }
-})
+  formContainer: {
+    height: '100%',
+    paddingVertical: '8%',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: '600',
+    marginBottom: '5%',
+  },
+  inputStyle: {
+    borderWidth: 1,
+    width: '100%',
+    borderRadius: 10,
+    borderColor: '#673AB7',
+    marginVertical: '2%',
+    paddingHorizontal: '3%',
+  },
+  inputTitle: {
+    color: '#000',
+    alignSelf: 'flex-start',
+    fontWeight: '600',
+  },
+});
