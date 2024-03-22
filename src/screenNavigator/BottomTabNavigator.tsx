ca0030c52985bcfe6f24ce9ce2281d957371201b
vaibhav164 <vai1641996@gmail.com>
2024-03-22 10:32:24 +0530

    Formated BottomabNaivgator.tsx using Priettier

--- src/screenNavigator/BottomTabNavigator.tsx
+++ src/screenNavigator/BottomTabNavigator.tsx
@@ -1,27 +1,68 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
-import { WorkspaceScreen } from '../screens/WorkSpaceScreen/WorkSpaceScreen';
+import {WorkspaceScreen} from '../screens/WorkSpaceScreen/WorkSpaceScreen';
 import Home from 'react-native-vector-icons/Entypo';
 import Icon from 'react-native-vector-icons/Fontisto';
 import OfficeIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import DocumetIcon from 'react-native-vector-icons/AntDesign';
-import { LogOutScreen } from '../screens/LogOutScreen/LogOutScreen';
-import { LeaveScreen } from '../screens/HomeScreen/LeaveScreen';
+import {LogOutScreen} from '../screens/LogOutScreen/LogOutScreen';
+import {LeaveScreen} from '../screens/HomeScreen/LeaveScreen';
 const Tab = createBottomTabNavigator();
 
-export const BottomTabs=()=> {
+export const BottomTabs = () => {
   return (
-    <Tab.Navigator screenOptions={() => ({
+    <Tab.Navigator
+      screenOptions={() => ({
         headerShown: false,
         gestureEnabled: true,
-    })}
-    >
-      <Tab.Screen options={{tabBarIcon:()=><Home size={30} color={'#B4B4B8'} name='home'/>,  tabBarStyle: {paddingBottom:'2.5%',height:'7.5%'}}} name="Home" component={WorkspaceScreen} />
-      <Tab.Screen options={{tabBarIcon:()=><OfficeIcon size={30} color={'#B4B4B8'} name='office-building-marker'/>}} name="Attendance" component={HomeScreen} />
-      <Tab.Screen options={{tabBarIcon:()=><DocumetIcon size={30} color={'#B4B4B8'} name='copy1'/>}} name="Documents" component={HomeScreen} />
-      <Tab.Screen options={{tabBarIcon:()=><Icon size={30} color={'#B4B4B8'} name='holiday-village'/>}} name="Leaves" component={LeaveScreen} />
-      <Tab.Screen options={{tabBarIcon:()=><Home size={30} color={'#B4B4B8'} name='user'/>}} name="Profile" component={LogOutScreen} />
+      })}>
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => <Home size={30} color={'#B4B4B8'} name="home" />,
+          tabBarStyle: {paddingBottom: '2.5%', height: '7.5%'},
+        }}
+        name="Home"
+        component={WorkspaceScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => (
+            <OfficeIcon
+              size={30}
+              color={'#B4B4B8'}
+              name="office-building-marker"
+            />
+          ),
+        }}
+        name="Attendance"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => (
+            <DocumetIcon size={30} color={'#B4B4B8'} name="copy1" />
+          ),
+        }}
+        name="Documents"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => (
+            <Icon size={30} color={'#B4B4B8'} name="holiday-village" />
+          ),
+        }}
+        name="Leaves"
+        component={LeaveScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: () => <Home size={30} color={'#B4B4B8'} name="user" />,
+        }}
+        name="Profile"
+        component={LogOutScreen}
+      />
     </Tab.Navigator>
   );
-}
+};
