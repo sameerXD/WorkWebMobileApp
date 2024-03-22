4db58b43c7b75c79b3d9c5b869d89faad81d4e3a
vaibhav164 <vai1641996@gmail.com>
2024-03-22 10:31:52 +0530

    Added feature for Date Selection in ApplyLeave

--- src/Components/ApplyLeaveForm.tsx
+++ src/Components/ApplyLeaveForm.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import Button from './Button';
 import {FormDropDown} from './FormDropDown';
-import {leaveHistoyStatusOptions} from '../assets/constants';
+import {dateSetSelection, leaveHistoyStatusOptions} from '../assets/constants';
+import DatePicker from 'react-native-date-picker';
 interface ApplyFormProps {
   title: string;
   handleSubmit: () => void;
 }
+interface formValueType {
+  leaveType: '';
+  leaveFromDate: '';
+  leaveTillDate: '';
+  applyTo: '';
+  ccTo: string[];
+  alternateNumber: '';
+  reason: '';
+}
 export const ApplyLeaveForm = ({title, handleSubmit}: ApplyFormProps) => {
+  const [date, setDate] = useState(new Date());
+  const [activeDateCalender, setActiveDateCalender] = useState('');
+  const [minimumDate, setMinimumDate] = useState<Date>();
+  const [formValues, setFormValues] = useState<formValueType>({
+    leaveType: '',
+    leaveFromDate: '',
+    leaveTillDate: '',
+    applyTo: '',
+    ccTo: [],
+    alternateNumber: '',
+    reason: '',
+  });
   return (
     <View style={styles.formContainer}>
       <ScrollView style={{flex: 0.9, width: '100%'}}>
@@ -24,8 +46,13 @@ export const ApplyLeaveForm = ({title, handleSubmit}: ApplyFormProps) => {
           placeholder="DD/MM/YYYY"
           placeholderTextColor={'#000'}
           onChangeText={val => {
-            console.log(val);
+            setActiveDateCalender(dateSetSelection.fromDate);
           }}
+          onFocus={() => {
+            setActiveDateCalender(dateSetSelection.fromDate);
+            console.log('focutedsed_____', 'leaveTillDate');
+          }}
+          value={formValues.leaveFromDate}
           style={styles.inputStyle}
         />
         <Text style={styles.inputTitle}>{'To Date'}</Text>
@@ -33,8 +60,13 @@ export const ApplyLeaveForm = ({title, handleSubmit}: ApplyFormProps) => {
           placeholder="DD/MM/YYYY"
           placeholderTextColor={'#000'}
           onChangeText={val => {
-            console.log(val);
+            setActiveDateCalender(dateSetSelection.tillDate);
           }}
+          onFocus={() => {
+            setActiveDateCalender(dateSetSelection.tillDate);
+            console.log('focutedsed_____', 'leaveTillDate');
+          }}
+          value={formValues.leaveTillDate}
           style={styles.inputStyle}
         />
         <Text style={styles.inputTitle}>{'Apply To'}</Text>
@@ -76,6 +108,42 @@ export const ApplyLeaveForm = ({title, handleSubmit}: ApplyFormProps) => {
         />
       </ScrollView>
       <Button handleSubmit={handleSubmit} title="Apply" size="m" />
+      <DatePicker
+        modal
+        open={activeDateCalender.length > 0}
+        minimumDate={minimumDate}
+        date={date}
+        onConfirm={date => {
+          setDate(date);
+          if (activeDateCalender == dateSetSelection.fromDate) {
+            setFormValues({
+              ...formValues,
+              leaveFromDate: `${date.getDate()}/${
+                date.getMonth() + 1
+              }/${date.getFullYear()}`,
+            });
+          } else if (activeDateCalender == dateSetSelection.tillDate) {
+            setFormValues({
+              ...formValues,
+              leaveTillDate: `${date.getDate()}/${
+                date.getMonth() + 1
+              }/${date.getFullYear()}`,
+            });
+          }
+          setActiveDateCalender('');
+        }}
+        onCancel={() => {
+          if (activeDateCalender == dateSetSelection.fromDate) {
+            setFormValues({...formValues, leaveFromDate: ''});
+          }
+          if (activeDateCalender == dateSetSelection.tillDate) {
+            setFormValues({...formValues, leaveTillDate: ''});
+          }
+          setActiveDateCalender('');
+        }}
+        style={{zIndex: 100}}
+        mode={'date'}
+      />
     </View>
   );
 };
@@ -87,6 +155,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fff',
     paddingHorizontal: '5%',
+    zIndex: -1,
   },
   title: {
     fontSize: 20,
@@ -95,12 +164,14 @@ const styles = StyleSheet.create({
     marginBottom: '5%',
   },
   inputStyle: {
-    borderWidth: 1,
+    borderWidth: 1.5,
     width: '100%',
     borderRadius: 10,
     borderColor: '#673AB7',
     marginVertical: '2%',
     paddingHorizontal: '3%',
+    fontSize: 15,
+    fontWeight: '600',
   },
   inputTitle: {
     color: '#000',
