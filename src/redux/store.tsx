2a66023871c5c1913b212ce8a5fdcedd0384b348
vaibhav164 <vai1641996@gmail.com>
2024-03-22 15:17:06 +0530

    Added Employeel Reducer to Store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import userReducer from './reducers/userReducer';
+import employeeListReducer from './reducers/employeeListReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  employeeList: employeeListReducer,
 });
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
 });
 
 export default store;
