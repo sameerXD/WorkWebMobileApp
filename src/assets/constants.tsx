b4c3a95bbd01e59a85718bb966c86e2b0b63ea7d
vaibhav164 <vai1641996@gmail.com>
2024-03-22 10:31:23 +0530

    Formated Constant File

--- src/assets/constants.tsx
+++ src/assets/constants.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { Dimensions } from "react-native";
+import React from 'react';
+import {Dimensions} from 'react-native';
 
 export const ScreenWidth = Dimensions.get('window').width;
 export const ScreenHeight = Dimensions.get('window').height;
 
+export const leaveHistoyStatusOptions = [
+  'Select',
+  'Pending',
+  'Approved',
+  'Declined',
+];
 
-export const leaveHistoyStatusOptions = ['Select','Pending', 'Approved', 'Declined']
+export const dateSetSelection = {
+  fromDate: 'FromDate',
+  tillDate: 'TillDate',
+};
