cff7e42976801eee26c1f410afe40be72bd9b898
vaibhav164 <vai1641996@gmail.com>
2024-03-28 12:39:29 +0530

    Formated LeaveHearty Api call file

--- src/redux/actions/getLeaveHistory.tsx
+++ src/redux/actions/getLeaveHistory.tsx
@@ -1,19 +1,13 @@
-import axios from "axios";
-import Config from "react-native-config";
+import axios from 'axios';
+import Config from 'react-native-config';
 interface getLeaveHistoryProps {
-    token: string;
+  token: string;
 }
-export const getLeaveHistory =async (token:getLeaveHistoryProps)=>{
-    const response = await axios.get(
-        `${Config.BASE_URL}/user/getLeaveHistory`,
-        {
-            headers: {
-                Authorization: token,
-            },
-        }
-    )
-    return response.data
-    console.log("leaveScreen Called",Config.BASE_URL);
-
-    
-}
+export const getLeaveHistory = async (token: getLeaveHistoryProps) => {
+  const response = await axios.get(`${Config.BASE_URL}/user/getLeaveHistory`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+  return response.data;
+};
