cfb63f85f98256567ff5a692ba65fd47d277cedd
vaibhav164 <vai1641996@gmail.com>
2024-03-28 12:39:09 +0530

    Added Api call for Apply LEaves

--- src/redux/actions/getEmployeeList.tsx
+++ src/redux/actions/getEmployeeList.tsx
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+import axios, {AxiosError} from 'axios';
 import {Alert} from 'react-native';
 import Config from 'react-native-config';
 
@@ -17,5 +17,47 @@ export const getAllEmployeeList = async () => {
     return response;
   } catch (error) {
     Alert.alert('Error', error);
+    // console.log('Error_________', error);
+  }
+};
+export interface postLeave {
+  leaveType: string;
+  toDate: string;
+  fromDate: string;
+  applyTo: string;
+  ccTo: string[] | [];
+  contactDetails: string;
+  reason: string;
+  status?: number;
+  jwt?: string;
+  applyFor?: string;
+}
+
+export const applyLeave = async (data: postLeave) => {
+  try {
+    const JwtToken = await AsyncStorage.getItem('token');
+    const putData = await axios.post(
+      `${Config.BASE_URL}/user/applyLeave`,
+      data,
+      {
+        headers: {
+          Authorization: JwtToken,
+        },
+      },
+    );
+    return putData.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        console.log('Error status:', axiosError.response.status);
+        console.log('Error data:', axiosError.response.data);
+        return axiosError.response.data;
+      } else {
+        return error;
+      }
+    } else {
+      return error;
+    }
   }
 };
